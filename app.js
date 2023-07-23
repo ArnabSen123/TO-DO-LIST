ed3f4734828c05447068a40f6968fdc90b9364d8
ArnabSen123 <73255755+ArnabSen123@users.noreply.github.com>
2023-07-23 15:47:35 +0600

    Home page and Register page

--- app.js
+++ app.js
@@ -1,75 +1,41 @@
-const express = require('express')
-const mysql = require('mysql')
+const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
 
-const app = express()
-const db = mysql.createPool({
-   connectionLimit: 100,
-   host: "127.0.0.1",       //This is your localhost IP
-   user: "root",         // "newuser" created in Step 1(e)
-   password: "",  // password for the new user
-   database: "userDB",      // Database name
-   port: "3306"             // port name, "3306" by default
-})
-db.getConnection( (err, connection)=> {
-   if (err) throw (err)
-   console.log ("DB connected successful: " + connection.threadId)
-})
+dotenv.config({path: './env'});
 
-const port = process.env.PORT
-app.listen(port, 
-()=> console.log(`Server Started on port ${port}...`))
+const app = express();
 
-const bcrypt = require("bcrypt")
-app.use(express.json())
-//middleware to read req.body.<params>
-//CREATE USER
-app.post("/createUser", async (req,res) => {
-const user = req.body.name;
-const hashedPassword = await bcrypt.hash(req.body.password,10);
-db.getConnection( async (err, connection) => {
- if (err) throw (err)
- const sqlSearch = "SELECT * FROM userTable WHERE user = ?"
- const search_query = mysql.format(sqlSearch,[user])
- const sqlInsert = "INSERT INTO userTable VALUES (0,?,?)"
- const insert_query = mysql.format(sqlInsert,[user, hashedPassword])
- // ? will be replaced by values
- // ?? will be replaced by string
- await connection.query (search_query, async (err, result) => {
-  if (err) throw (err)
-  console.log("------> Search Results")
-  console.log(result.length)
-  if (result.length != 0) {
-   connection.release()
-   console.log("------> User already exists")
-   res.sendStatus(409) 
-  } 
-  else {
-   await connection.query (insert_query, (err, result)=> {
-   connection.release()
-   if (err) throw (err)
-   console.log ("--------> Created new User")
-   console.log(result.insertId)
-   res.sendStatus(201)
-  })
- }
-}) //end of connection.query()
-}) //end of db.getConnection()
-}) //end of app.post()
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password1#',
+    database: 'login'
+});
 
-// env file
-require("dotenv").config()
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_DATABASE = process.env.DB_DATABASE
-const DB_PORT = process.env.DB_PORT
-const db = mysql.createPool({
-   connectionLimit: 100,
-   host: DB_HOST,
-   user: DB_USER,
-   password: DB_PASSWORD,
-   database: DB_DATABASE,
-   port: DB_PORT
-})
+const publicDirectory = path.join(__dirname,'./public')
+app.use(express.static(publicDirectory));
 
+app.set('view engine', 'hbs');
 
+db.connect((error) => {
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("MySQL Connected...")
+    }
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.listen(5000, () =>{
+    console.log("Server started on port 5000");
+});
